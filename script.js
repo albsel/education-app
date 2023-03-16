4c06d42d1f5ecd32a6242a07dc7dbaf0b6668979
albsel <alban_selimi@yahoo.com>
2023-03-16 18:22:57 +0100

    home page

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+// Get navbar background on scroll page
 window.addEventListener("scroll", () => {
   document
     .querySelector("nav")
     .classList.toggle("window-scroll", window.scrollY > 0);
+});
+
+// Toggel the frequently asked questions div
+const faqsEl = document.querySelectorAll(".faq");
+
+faqsEl.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+
+    // change icon
+    const icon = faq.querySelector(".faq__icon i");
+    if (icon.className === "fa-solid fa-plus") {
+      icon.className = "fa-solid fa-minus";
+    } else {
+      icon.className = "fa-solid fa-plus";
+    }
+  });
 });
